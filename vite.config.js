707fd2d22c8bc6063ac8c675c3e2a7bbf2b639be
Tristan <darkskiiittles@gmail.com>
2024-09-13 05:53:07 -0600

    settings update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets', // Directory for placing static assets in dist directory. Default is "assets".
+    rollupOptions: {
+      output: {
+        // Keeping a consistent naming structure
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      }
+    }
+  }
 })
